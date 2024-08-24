d42b896532356e1078a2db88100ceeca032aac33
uzgorenm <mehmetuzgoren@gmail.com>
2024-08-24 15:45:48 -0500

    Updates

---
+++ precache-manifest.7a9b32815aeeb991f13af4950e2d1d87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62665281bae8145de3a96ee4bd5cee17",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd1b2e365e9d83887050",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "aa1cfac72de38a3723a4",
+    "url": "/static/css/main.db6f0745.chunk.css"
+  },
+  {
+    "revision": "dd1b2e365e9d83887050",
+    "url": "/static/js/2.ab137e78.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.ab137e78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa1cfac72de38a3723a4",
+    "url": "/static/js/main.831958d8.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
